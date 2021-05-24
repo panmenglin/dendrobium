e72f20f16171ffbc0a32596e1bde8fd9afa52c64
msx.pan <msx.pan@outlook.com>
2021-05-24 18:18:22 +0800

    fix: fix like action buf

--- view/material/material.js
+++ view/material/material.js
@@ -1,5 +1,5 @@
 /**
- * material list
+ * conponents list
  */
 const vscode = acquireVsCodeApi();
 let intl = {};
@@ -30,12 +30,17 @@ window.addEventListener('message', (event) => {
     $('.search input').show();
 
     // tab
+    selectedLibrary = message.library[0];
     $('.tabs-item').click(function () {
       $('#selectedLibrary').html($(this).html());
       $(this).addClass('active').siblings().removeClass('active');
       $('.loading').show();
 
+
       selectedLibrary = message.library[$(this).attr('key')];
+
+      console.log(selectedLibrary);
+
       vscode.postMessage({ warehouseSelected: selectedLibrary });
     });
   }
@@ -46,16 +51,9 @@ window.addEventListener('message', (event) => {
     $('.search input').attr('placeholder', intl['searchPlaceholder']);
   }
 
-  // block
+  // library
   if (message.components) {
     let blocks = message.components;
-
-    // blocks = JSON.parse(JSON.stringify(blocks));
-
-    // // blocks.map((item, index) => {
-    // //   item.title = `${item.title}${index}`;
-    // // });
-
     blockListRender(blocks);
     bindSearch(blocks);
   }
@@ -85,7 +83,7 @@ function bindSearch(blocks) {
 function blockListRender(blocks) {
   // pagination
   let pageNo = 1;
-  const pageSize = 2;
+  const pageSize = 5;
   let maxPageNo = Math.ceil(blocks.length / pageSize);
 
   let pagination =
@@ -161,7 +159,7 @@ function blockListRender(blocks) {
                           </svg>
                         </span>
                         <span style="margin-right: 8px">12</span>
-                        <span class="op-like">
+                        <span class="op-like" itemIndex="${ beginIndex + index }">
                           <svg t="1620972750303" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7819" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="16">
                               <path d="M749.624889 980.152889a95.601778 95.601778 0 0 1-45.084445-11.306667l-192.526222-103.637333-192.469333 103.608889c-31.203556 16.64-70.314667 14.392889-99.043556-5.304889-29.511111-20.337778-44.743111-55.921778-38.840889-90.695111l37.703112-225.251556-162.702223-162.474666c-25.002667-24.888889-33.464889-60.984889-22.058666-94.193778 11.349333-32.910222 40.064-56.576 74.965333-61.752889l221.326222-32.896 96.128-199.580445c15.488-32.085333 48.853333-52.807111 84.992-52.807111 36.167111 0 69.518222 20.736 84.963556 52.807111l96.156444 199.580445 221.297778 32.910222c34.872889 5.162667 63.616 28.8 74.979556 61.696a90.666667 90.666667 0 0 1-22.030223 94.250667L804.622222 647.566222l37.745778 225.28c5.845333 34.773333-9.386667 70.357333-38.812444 90.638222a95.274667 95.274667 0 0 1-53.930667 16.668445zM512.014222 804.451556c4.636444 0 9.272889 1.137778 13.482667 3.384888l205.937778 110.862223a39.253333 39.253333 0 0 0 39.936-2.133334c11.392-7.836444 17.123556-21.006222 14.890666-34.304L746.097778 642.503111a28.472889 28.472889 0 0 1 7.964444-24.832l173.141334-172.842667a34.147556 34.147556 0 0 0 8.405333-35.484444c-4.380444-12.672-15.701333-21.873778-29.525333-23.921778L669.866667 350.293333a28.430222 28.430222 0 0 1-21.447111-15.786666L545.720889 121.358222c-6.030222-12.529778-19.256889-20.608-33.706667-20.608s-27.690667 8.092444-33.763555 20.622222L375.608889 334.506667a28.430222 28.430222 0 0 1-21.447111 15.786666L117.930667 385.422222c-13.624889 2.033778-25.201778 11.434667-29.539556 23.992889-4.266667 12.416-1.024 25.984 8.433778 35.399111L269.937778 617.671111c6.513778 6.499556 9.472 15.744 7.964444 24.832l-40.135111 239.744c-2.247111 13.312 3.484444 26.439111 14.976 34.360889 11.434667 7.836444 27.349333 8.775111 39.950222 2.062222l205.852445-110.833778c4.195556-2.247111 8.832-3.384889 13.468444-3.384888z" p-id="7820" fill="${
                                 item.like ? '#FFB531' : '#e6e6e6'
@@ -185,7 +183,7 @@ function blockListRender(blocks) {
                       <div class="block-operation">
                           <a href="javascript:;" class="op-add" itemIndex="${
                             beginIndex + index
-                          }">${intl['add'] || '添加'}</a>   
+                          }">${intl['add'] || '添加'}</a>
                           <a href="${
                             item.previewUrl || item.url
                           }" target="_blank">${intl['preview'] || '文档'}</a>
@@ -205,43 +203,15 @@ function blockListRender(blocks) {
       selected.warehouse = selectedLibrary;
 
       vscode.postMessage({ componentLike: selected });
-
     });
 
-    // block click
+    // component click
     $('.op-add').click(function () {
       const itemIndex = $(this).attr('itemIndex');
       const selected = blocks[itemIndex];
       selected.warehouse = selectedLibrary;
 
-      // if (selected.type === "npm") {
-      //   $(".shadow").show();
-
-      //   // insert project
-      //   $("#leftButton")
-      //     .off()
-      //     .click(function () {
-      //       vscode.postMessage({
-      //         blockSelected: {
-      //           ...selected,
-      //           type: "snippet",
-      //         },
-      //       });
-
-      //       $(".shadow").hide();
-      //     });
-
-      //   // as npm dependencies
-      //   $("#rightButton")
-      //     .off()
-      //     .click(function () {
-      //       vscode.postMessage({ blockSelected: selected });
-
-      //       $(".shadow").hide();
-      //     });
-      // } else {
       vscode.postMessage({ blockSelected: selected });
-      // }
     });
 
     $('#close').click(function () {
